9d188b4412edc20ab55d8b6fb295fc9bac41e35c
Yury Korziuk <yurko@tut.by>
2022-12-17 13:36:56 +0300

    Commit 1. First init.

---
+++ README.md
@@ -0,0 +1,9 @@
+### Receipt Create CLI App
+
+***
+
+It`s just test project 🛒 from the Clevertec 
+
+### Used:
+
+***
---
+++ src/main/java/by/korziuk/check_app/CheckRunner.java
@@ -0,0 +1,11 @@
+package by.korziuk.check_app;
+
+public class CheckRunner {
+
+    public String init() {
+        return "init CheckRunner ...";
+    }
+    public static void main(String[] args) {
+        System.out.println(new CheckRunner().init());
+    }
+}
